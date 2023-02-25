075ccdd0b772c1e2c98e28678bd07cf3a6e96177
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-02-25 13:36:02 +0200

    marvelapp_full_nohooks

---
+++ src/components/app/App.js
@@ -0,0 +1,47 @@
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import { Component } from "react";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import decoration from '../../resources/img/vision.png';
+
+
+class App extends Component {
+
+    state = {
+    selectedChar: null,
+    activeChar: null
+}
+
+
+onCharSelected = (id,ref) =>{
+    this.setState({
+        selectedChar: id
+    })
+ 
+}
+
+render(){
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main><ErrorBoundary><RandomChar/></ErrorBoundary>
+                <div className="char__content">
+                    <ErrorBoundary>
+                         <CharList onCharSelected ={this.onCharSelected}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharInfo charId = {this.state.selectedChar}/>
+                    </ErrorBoundary>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main>
+        </div>
+    )
+}
+
+}
+
+
+export default App;
